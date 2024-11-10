f80f38cbd724160e43314e63be00cde44e2feb78
Dagm Kebede <dagmawe2002@gmail.com>
2024-11-09 19:28:01 -0500

    Separate folders

--- README.md
+++
@@ -1,3 +0,0 @@
-# financeflow
-# financeflow
-# financeflow
--- financeflow-frontend/src/config.js
+++
@@ -1,6 +0,0 @@
-// src/config.js
-export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
-
-if (!API_BASE_URL) {
-  throw new Error("API_BASE_URL is not defined. Please set it in your environment variables.");
-}
